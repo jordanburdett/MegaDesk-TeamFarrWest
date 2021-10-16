2f235be465617362f0067a00007d6e227828b635
Jordan Burdett <jordan@burdett.us>
2021-10-15 22:31:30 -0600

    Implemented search quote data view with filtered quotes

--- MegaDesk-Burdett/Views/SearchQuotes.Designer.cs
+++ MegaDesk-Burdett/Views/SearchQuotes.Designer.cs
@@ -31,7 +31,11 @@
             this.BackButton = new System.Windows.Forms.Button();
             this.searchLabel = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.MaterialComboBox = new System.Windows.Forms.ComboBox();
+            this.SearchQuotesResults = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SearchQuotesErrorText = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.SearchQuotesResults)).BeginInit();
             this.SuspendLayout();
             // 
             // BackButton
@@ -64,32 +68,67 @@
             this.label1.TabIndex = 4;
             this.label1.Text = "By Desk Material";
             // 
-            // comboBox1
+            // MaterialComboBox
             // 
-            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
+            this.MaterialComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.MaterialComboBox.FormattingEnabled = true;
+            this.MaterialComboBox.Items.AddRange(new object[] {
             "laminate",
             "oak",
             "rosewood",
             "veneer",
             "pine"});
-            this.comboBox1.Location = new System.Drawing.Point(357, 146);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 5;
+            this.MaterialComboBox.Location = new System.Drawing.Point(357, 128);
+            this.MaterialComboBox.Name = "MaterialComboBox";
+            this.MaterialComboBox.Size = new System.Drawing.Size(121, 21);
+            this.MaterialComboBox.TabIndex = 5;
+            this.MaterialComboBox.SelectedValueChanged += new System.EventHandler(this.OnValueSelected);
+            // 
+            // SearchQuotesResults
+            // 
+            this.SearchQuotesResults.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.SearchQuotesResults.Location = new System.Drawing.Point(197, 170);
+            this.SearchQuotesResults.Name = "SearchQuotesResults";
+            this.SearchQuotesResults.ReadOnly = true;
+            this.SearchQuotesResults.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.SearchQuotesResults.Size = new System.Drawing.Size(441, 207);
+            this.SearchQuotesResults.TabIndex = 11;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(666, 405);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 33);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "View Quote Details";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // SearchQuotesErrorText
+            // 
+            this.SearchQuotesErrorText.AutoSize = true;
+            this.SearchQuotesErrorText.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SearchQuotesErrorText.ForeColor = System.Drawing.Color.IndianRed;
+            this.SearchQuotesErrorText.Location = new System.Drawing.Point(340, 394);
+            this.SearchQuotesErrorText.Name = "SearchQuotesErrorText";
+            this.SearchQuotesErrorText.Size = new System.Drawing.Size(0, 17);
+            this.SearchQuotesErrorText.TabIndex = 12;
             // 
             // SearchQuotes
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.SearchQuotesErrorText);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.SearchQuotesResults);
+            this.Controls.Add(this.MaterialComboBox);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.searchLabel);
             this.Controls.Add(this.BackButton);
             this.Name = "SearchQuotes";
             this.Text = "SearchQuotes";
+            ((System.ComponentModel.ISupportInitialize)(this.SearchQuotesResults)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -100,6 +139,9 @@
         private System.Windows.Forms.Button BackButton;
         private System.Windows.Forms.Label searchLabel;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox MaterialComboBox;
+        private System.Windows.Forms.DataGridView SearchQuotesResults;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label SearchQuotesErrorText;
     }
 }
--- MegaDesk-Burdett/Views/SearchQuotes.cs
+++ MegaDesk-Burdett/Views/SearchQuotes.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MegaDesk_Burdett.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -24,6 +25,49 @@ namespace MegaDesk_Burdett
             mainMenuView.Show();
         }
 
-      
+        private void OnValueSelected(object sender, EventArgs e)
+        {
+            var selection = MaterialComboBox.SelectedIndex;
+
+            var quotes = DataStorage.getQuotes();
+
+            var filteredQuotes = quotes.FindAll(quote => (int)quote.desk.DesktopMaterial == selection);
+            
+
+            SearchQuotesResults.DataSource = filteredQuotes;
+
+            SearchQuotesResults.Columns["desk"].Visible = false;
+            SearchQuotesResults.Columns["quoteDetails"].Visible = false;
+
+            SearchQuotesResults.Columns["customerName"].HeaderText = "Name";
+            SearchQuotesResults.Columns["deliveryOptions"].HeaderText = "Rush Order";
+            SearchQuotesResults.Columns["totalPrice"].HeaderText = "Price";
+
+            SearchQuotesResults.Columns["dateDisplayString"].HeaderText = "Date";
+
+            // to allow each column to be sortable.
+            foreach (DataGridViewColumn column in SearchQuotesResults.Columns)
+            {
+                column.SortMode = DataGridViewColumnSortMode.Automatic;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var selectedQuote = SearchQuotesResults.SelectedRows[0].DataBoundItem as DeskQuote;
+
+                var viewDetails = new DisplayQuotes();
+                viewDetails.Tag = this;
+                viewDetails.showQuote(selectedQuote);
+                viewDetails.Show();
+                this.Hide();
+            }
+            catch
+            {
+                SearchQuotesErrorText.Text = "Please select a quote!";
+            }
+        }
     }
 }
